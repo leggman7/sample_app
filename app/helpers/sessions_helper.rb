879f22d35a3df3813813a8364b12a26b5e8692ca
leggman7 <leggman7@gmail.com>
2023-02-28 20:20:38 -0700

    change website over to Go Adventure words

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,18 @@
+module SessionsHelper
+  # logs in the given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # returns the current logged in user
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  # returns true if the user is logged in, else false
+  def logged_in?
+    !current_user.nil?
+  end
+end
